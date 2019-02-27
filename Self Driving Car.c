1f380091a2dd8d1dbf03415f68d67e27cd5a7ccb
aditya174 <47366648+aditya174@users.noreply.github.com>
2019-02-27 11:51:36 -0800

    Add files via upload

---
+++ Self Driving Car.c
@@ -0,0 +1,46 @@
+
+/*
+Project Title:
+Team Members:
+Date:
+Section:
+
+
+Task Description:
+
+
+Pseudocode:
+
+var declared at the start for push button
+
+while(ultrasonic sensor is reading more than 15cm)
+	move forward
+when the ultrasonic sensor reads less than 15
+	move left wheel forward and right backward
+	wait 1 second
+	move forward
+repeat
+
+flashlight
+if lightsenor reads less than x
+	then start light
+else
+	stop light
+loop that
+
+
+always (while)
+sentinal
+when push button pressed change var to one and break code
+
+
+*/
+
+task main(){                                     //Program begins, insert code within curly braces
+	int pushButtton = 0;
+
+	while(1){
+	}
+
+
+}
